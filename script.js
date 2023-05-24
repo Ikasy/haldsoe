a50619b803893ccabc6b3c27cd59442c5194e725
Ikasy <jlksn@live.dk>
2023-05-24 16:14:06 +0200

    tested new slider, fixed minor things

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-$('.ads_sponsors').slick({
-	autoplay: true,
-	autoplaySpeed: 1000,
-	slidesToShow: 4,
-    slidesToScroll: 1,
-    arrows: false
-});
+
+function prev(){
+		document.getElementById('slider-container').scrollLeft -= 600;
+}
+
+function next()
+{
+		document.getElementById('slider-container').scrollLeft += 600;
+}
---
+++ scripttest.js
@@ -0,0 +1,7 @@
+$('.slides').slick({
+	autoplay: true,
+	autoplaySpeed: 5000,
+	slidesToShow: 2.7,
+    slidesToScroll: 1,
+    arrows: false
+});
